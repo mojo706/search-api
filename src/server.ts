6d17f1a723e4c1751250b303f6a2b399e7b36224
mojo706 <xriseugene@gmail.com>
2020-07-27 09:39:52 +0300

    add a bit of error handling, gonna test soon

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,16 @@ import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
 import routes from "./services";
 
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
+
 const router = express();
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
